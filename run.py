9808c97b911a265984447cb5b1325090421499e7
James Peres <james.peres.s@gmail.com>
2019-03-05 16:54:49 -0300

    feature(app): Cria um factory para app e adiona .env

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from app.load import create_app
+from decouple import config
 
-app = Flask(__name__)
-
+app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=config("DEBUG"))
