2c4519198b7fca297bca94d8a6024d67cb3a421b
James Peres <james.peres.s@gmail.com>
2019-03-05 19:43:33 -0300

    feature(views): Adiciona MovieDetail e MovieRegister

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api
-from app.views import MovieList
+from app.views import MovieList, MovieDetail, MovieRegister
 
 
 api_bp = Blueprint('api', __name__)
 movies = Api(api_bp, prefix="/v1")
 
 movies.add_resource(MovieList, '/movies')
+movies.add_resource(MovieDetail, '/movies/<movie_id>')
+movies.add_resource(MovieRegister, '/movies/register')
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import jsonify
+from flask import jsonify, current_app, request
 from flask_restful import Resource
-from app.models import Movie, MovieSchema
+from app.models import Movie, MovieSchema, Cast
 
 
 class MovieList(Resource):
@@ -10,3 +10,44 @@ class MovieList(Resource):
         movies_schema = MovieSchema(many=True)
         output = movies_schema.dump(movies).data
         return {'movies' : output}
+
+class MovieDetail(Resource):
+
+    def get(self, movie_id):
+        movies = Movie.query.filter(Movie.id == movie_id)
+        movies_schema = FilmSchema(many=True)
+        output = movies_schema.dump(movies).data
+        return {'movie' : output}
+
+class MovieRegister(Resource):
+
+    def post(self):
+        try:
+            movie = self.register_movie(request.json)
+            movie = self.register_cast(request.json, movie)    
+            movie_schema = MovieSchema()
+            output = movie_schema.dump(movie).data
+        except Exception as e:
+            return str(e), 400
+        return 'ok', 201
+
+    def register_movie(self, json):
+        title = json['title']
+        brazilian_title = json['brazilian_title']
+        year_of_production = json['year_of_production']
+        director = json['director']
+        genre = json['genre']
+        movie = Movie(title=title, brazilian_title=brazilian_title, year_of_production=year_of_production,
+                        director=director, genre=genre)
+        current_app.db.session.add(movie)
+        current_app.db.session.commit()
+        return movie
+
+    def register_cast(self, json, movie):
+        for cast in json['cast']:
+            role = cast['role']
+            name = cast['name']
+            cast_info = Cast(role=role, name=name, movie_id=movie.id)
+            current_app.db.session.add(cast_info)
+            current_app.db.session.commit()
+        return movie
